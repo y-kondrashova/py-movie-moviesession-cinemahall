fc90a302d78265db98f61ca62395557c76cb4e39
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-04-22 19:12:10 +0300

    Refactored

--- db/models.py
+++ db/models.py
@@ -19,8 +19,8 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor, related_name='movies')
-    genres = models.ManyToManyField(Genre, related_name='movies')
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
         return self.title
@@ -32,7 +32,7 @@ class CinemaHall(models.Model):
     seats_in_row = models.IntegerField()
 
     @property
-    def capacity(self):
+    def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
     def __str__(self) -> str:
--- services/movie.py
+++ services/movie.py
@@ -6,7 +6,8 @@ from db.models import Movie
 def get_movies(genres_ids: list = None, actors_ids: list = None) -> QuerySet:
     queryset = Movie.objects.all()
     if genres_ids and actors_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids, actors__id__in=actors_ids)
+        queryset = queryset.filter(genres__id__in=genres_ids,
+                                   actors__id__in=actors_ids)
     elif genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
     elif actors_ids:
@@ -23,10 +24,11 @@ def get_movie_by_id(movie_id: int) -> Movie | None:
 
 
 def create_movie(movie_title: str,
-                 movie_description,
+                 movie_description: str,
                  genres_ids: list = None,
                  actors_ids: list = None) -> Movie:
-    movie = Movie.objects.create(title=movie_title, description=movie_description)
+    movie = Movie.objects.create(title=movie_title,
+                                 description=movie_description)
 
     if genres_ids:
         movie.genres.add(*genres_ids)
